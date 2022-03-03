83a9d00c485cf29b7b18239a8cbc0ae89d0551e4
AnnaYurova <94358663+AnnaYurova@users.noreply.github.com>
2022-03-03 13:25:00 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -24,14 +24,14 @@ uint64_t fact(uint16_t n) {
 }
 
 double calcItem(double x, uint16_t n) {
-  return (pown(x,n) / fact(n));
+  return (pown(x, n) / fact(n));
 }
 
 double expn(double x, uint16_t count) {
   double e = 0;
   uint64_t check = 0;
     while (check <= count) {
-      e += calcItem (x,check);
+      e += calcItem(x, check);
       check += 1;
     }
   return  e;
@@ -40,7 +40,7 @@ double expn(double x, uint16_t count) {
 double sinn(double x, uint16_t count) {
   double e = 0;
   for (uint16_t check = 1; check <= count; check++) {
-    e += pown (-1, check-1) * calcItem (x, (2 * check) - 1);
+    e += pown(-1, check-1) * calcItem(x, (2 * check) - 1);
   }
   return  e;
 }
@@ -49,7 +49,7 @@ double cosn(double x, uint16_t count) {
   double e = 0;
   uint64_t check = 1;
     while (check <= count) {
-      e += pown (-1, check - 1) * calcItem (x, (2 * check) - 2);
+      e += pown(-1, check - 1) * calcItem(x, (2 * check) - 2);
       check += 1;
     }
   return  e;
